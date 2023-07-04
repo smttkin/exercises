ff5e1e41c3d8c1c54bc062a7fb48b1bb3cad6a7c
smttkin <avestagata@gmail.com>
2023-07-05 02:10:49 +0300

    Visual Sorting Algorithm Exercise:Modify existing files and add/update comments

--- visual_sorting_algorithms/src/scripts/ArrayVisualizer.js
+++ visual_sorting_algorithms/src/scripts/ArrayVisualizer.js
@@ -11,13 +11,13 @@ class ArrayVisualizer {
       this.onresizecheck = null;
   
     }
-     calculateFactors(arr) {
-    var max = Math.max(...arr);
-    var min = Math.min(...arr);
-    var height_factor = Math.ceil(this.view_canvas.height / (max - min + 1));
-    var width_factor = Math.ceil(this.view_canvas.width / arr.length);
-    return { max, min, height_factor, width_factor };
-}
+    calculateFactors(arr) {
+      var max = Math.max(...arr);
+      var min = Math.min(...arr);
+      var height_factor = Math.ceil(this.view_canvas.height / (max - min +1));
+      var width_factor = Math.ceil(this.view_canvas.width / arr.length);
+      return { max, min, height_factor, width_factor };
+    }
     onReSize(e) {
       this.onresizecheck = e;
       this.view_container = document.getElementById("view-container");
@@ -49,37 +49,60 @@ class ArrayVisualizer {
       this.view_canvas = document.getElementById("view-canvas");
       this.view_canvas.clearRect(0, 0, this.view_canvas.width, this.view_canvas.height);
     }
-
+    
     async updateView(newArr = [1], color_arr = ["red"], each_elem_duration = 0.01, sleepms = 500, callback = (arr,color_arr,each_elem_duration,sleepms,index) => { }) {
-        //Object that allows to manipulate canvas context
-        var view_canvas_ctx = this.view_canvas.getContext('2d');
-        
-        //Some calculations 
-        let { min, max, height_factor, width_factor } =this.calculateFactors(newArr);
-      await sleep(sleepms);
-      view_canvas_ctx.clearRect(0, 0, this.view_canvas.width, this.view_canvas.height);
+      //Object that allows to manipulate canvas context
+      var view_canvas_ctx = this.view_canvas.getContext('2d');
       
-        callback(newArr,color_arr,each_elem_duration,sleepms,index);
+      //Some calculations about how array elements will be positioned and scaled as rectangle in canvas  
+      let { min, max, height_factor, width_factor } =this.calculateFactors(newArr);
+      
+      //Wait for sleepms milliseconds on each updateView frame
+      if(sleepms>0)
+        await sleep(sleepms);
 
-        for (var index = 0; index < newArr.length; index++) {
-          
+      //Cleaning canvas 
+      view_canvas_ctx.clearRect(0, 0, this.view_canvas.width, this.view_canvas.height);
+
+      //Additional things may user want to do with array
+      callback(newArr,color_arr,each_elem_duration,sleepms,index);
+
+      for (var index = 0; index < newArr.length; index++) {
+        
+        //Redraw the array elements onto the new scaled canvas 
+        //If onReSize function executed while updateView is still running
         if (this.onresizecheck) {
           this.onresizecheck = null;
           this.updateView(newArr, color_arr, each_elem_duration, sleepms);
           return;
         }
-          var val = newArr[index];
-          
-        view_canvas_ctx.fillStyle = (color_arr.length<newArr.length)?color_arr[Math.floor(color_arr.length * val / (max - min + 1))]:color_arr[index];
-        var x = (index-1) * width_factor;
-        var y = this.view_canvas.height - (height_factor * val);
-        var w = width_factor;
-        var h = val * height_factor;
-        view_canvas_ctx.fillRect(x, y, w, h);
-        if (each_elem_duration > 1)
-              await sleep(each_elem_duration);
         
-        }
+        //Wait for each element drawing if it is wanted
+        if (each_elem_duration > 0)
+          await sleep(each_elem_duration);
+        
+        //Current element
+        var val = newArr[index];
+
+        //Calculation of coordiante and scale values 
+        /*so we can draw all the elements properly into the canvas*/
+        var x = index * width_factor+index;
+        var y = this.view_canvas.height - (height_factor * (val));
+        var w = width_factor-1;
+        var h = val * height_factor;
+                
+        //Gathering current element's color from color_arr
+        /*
+          Special case: If there is less color info then newArr element count
+          The colors of array elements should be repeated periodically
+        */
+        view_canvas_ctx.fillStyle = (color_arr.length<newArr.length)?color_arr[index%(color_arr.length)]:color_arr[index];
+        
+        //Drawing the rectangle for the current element into the canvas
+        view_canvas_ctx.fillRect(x, y, w, h);
+
+   
+      }
   }
   
  
--- visual_sorting_algorithms/src/scripts/SoundPlayer.js
+++ visual_sorting_algorithms/src/scripts/SoundPlayer.js
@@ -14,6 +14,7 @@ class SoundPlayer {
         this.#initOscillator();
         this.multiplier = 1.0;
     }
+
     setFrequencies(arr=[432.0]) {
         if (arr.constructor == (Array || Float32Array || Float64Array)) {
             this.#frequencies = arr;
@@ -51,49 +52,68 @@ class SoundPlayer {
     setTimeMultiplier(factor=0.5) {
         this.multiplier = factor;
     }
+    
+    //When playNext reaches to end of the array set index pointer to the 0 
     resetPlay() {
         this.#index = 0;
     }
+
+    //Plays frequencies sequentially in the array 
     playNext() {
         if (this.#index < this.#frequencies.length)
             this.playFrequency(this.#frequencies[this.#index++]);
         else this.resetPlay();
     }
+    
+    //Plays one frequency at a time 
     playFrequency(frequency) {
+    
         this.#initOscillator();
+
+        //Copying properties of this object to use them in local scope of playFrequency method
         let { audioContext, oscillator, attackTime, decayTime, sustainLevel, releaseTime, gainNode } = this;
+        
         attackTime *= this.multiplier;
         decayTime *= this.multiplier;
         releaseTime *= this.multiplier;
         
-
-        // Gain düğümü oluştur
+        // Connecting gain node to the  destination
         const gain = audioContext.createGain();
         gain.connect(gainNode || audioContext.destination);
 
-        // Ses oynatma için gerekli bağlantıları oluştur
+        //Connecting the oscillator node to the gain so we can apply the ADSR envelope
         oscillator.connect(gain);
         oscillator.frequency.value = frequency;
 
-        // Saldırı
+        /*
+          ADSR
+          Attack,Decay,Sustain,Release
+        */
+
+        // Attack
         oscillator.start(audioContext.currentTime);
         gain.gain.setValueAtTime(0, audioContext.currentTime);
         gain.gain.linearRampToValueAtTime(1, audioContext.currentTime + attackTime);
 
-        // Sönümleme
+        // Decay
         gain.gain.linearRampToValueAtTime(sustainLevel, audioContext.currentTime + attackTime + decayTime);
 
-        // Sürdürme
+        // Sustain
         gain.gain.setValueAtTime(sustainLevel, audioContext.currentTime + attackTime + decayTime);
-        // Salınımla bitirme
+        
+        // Release
         gain.gain.linearRampToValueAtTime(0, audioContext.currentTime + attackTime + decayTime + releaseTime);
 
-        oscillator.stop(audioContext.currentTime+attackTime+decayTime+releaseTime);
+        //Stop the oscillator so sound is stopped 
+        oscillator.stop(audioContext.currentTime + attackTime + decayTime + releaseTime);
+        
+        //Releasing the memory
         delete this.oscillator;
     }
 }
+
 function allowAudio(event) {
-    console.log("allowAudio");
+
     if (event.target.parentElement != (document.body || document.html || window))
         event.target.parentElement.style.display = 'none';
     else
