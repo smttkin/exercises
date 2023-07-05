fc31a0105272e3c6627372b02eee0a3ed0db7f56
smttkin <avestagata@gmail.com>
2023-07-06 00:05:40 +0300

    Main menu added(currently not functional)

--- visual_sorting_algorithms/src/scripts/index.js
+++ visual_sorting_algorithms/src/scripts/index.js
@@ -1,5 +1,6 @@
 const index_controller = require("../controllers/index_controller");
-const scripts_controller=require("../controllers/scripts_controller")
+const scripts_controller = require("../controllers/scripts_controller");
+const styles_controller = require("../controllers/styles_controller");
 const express = require('express');
 const ejs = require('ejs');
 const fs = require('fs');
@@ -11,6 +12,7 @@ app.set('view engine', "ejs");
 app.set('views','src/views');
 app.get("/", index_controller)
 app.get('/scripts/*', scripts_controller);
+app.get('/styles/*', styles_controller);
 
 
 
